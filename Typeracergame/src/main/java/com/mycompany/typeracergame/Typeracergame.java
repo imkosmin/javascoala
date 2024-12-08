32bcb23673fd8527a3d5a1af8f63d8a0f61bf295
imkosmin <72415818+imkosmin@users.noreply.github.com>
2024-12-08 21:10:04 +0200

    Add files via upload

---
+++ Typeracergame/src/main/java/com/mycompany/typeracergame/Typeracergame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.typeracergame;
+
+/**
+ *
+ * @author hcosm
+ */
+public class Typeracergame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
